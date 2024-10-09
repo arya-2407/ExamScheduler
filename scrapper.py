679425caa300f02c90a0fd4a0793cfc27455d038
arya-2407 <aryapofficial@gmail.com>
2024-10-08 20:34:21 -0700

    replaced requests with selenium

--- scrapper.py
+++ scrapper.py
@@ -2,17 +2,25 @@ from selenium import webdriver
 
 url = 'https://heat.csc.uvic.ca/coview/course/2024091/CSC370?unp=t'
 
-# Use a headless browser to run in the background
+# Set up headless mode for Chrome with additional options
 options = webdriver.ChromeOptions()
-options.headless = True
+options.add_argument('--headless=new')  # Use the new headless mode if supported
+options.add_argument('--disable-gpu')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-extensions')
+options.add_argument('--hide-scrollbars')
+options.add_argument('--window-size=1920,1080')
+options.add_argument('--disable-software-rasterizer')
 
-# Adjust the path to your Chrome driver
 driver = webdriver.Chrome(options=options)
 driver.get(url)
 
 # Extract the page source
 html = driver.page_source
-print(html)
-
+print(type(html))
 
 driver.quit()
+
+"""f=open("output.txt","a")
+f.write(html)
+f.close()"""
