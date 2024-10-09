0c286a6b82756b208be524b11ff3d7b7b41192fb
arya-2407 <aryapofficial@gmail.com>
2024-10-09 14:15:21 -0700

    writing html to txt

--- scrapper.py
+++ scrapper.py
@@ -1,5 +1,16 @@
 import requests
 
+def fetch_html(url):
+    html_text=requests.get(url,verify=False).text
+    return html_text
+
+def write_file(text):
+    with open('output.txt','w',encoding='utf-8') as file:
+        file.write(text)
+    file.close()
+
+
+
 url='https://heat.csc.uvic.ca/coview/course/2024091/CSC370?unp=t'
-html_text=requests.get(url,verify=False).text
-print(html_text)
+text=fetch_html(url)
+write_file(text)
