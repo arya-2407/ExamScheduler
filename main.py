af1e44e561286984973e5c99ea05be377b5ec1b9
arya-2407 <aryapofficial@gmail.com>
2024-10-13 22:41:29 -0700

    implemented flask app to take user input

--- main.py
+++ main.py
@@ -2,31 +2,22 @@ import os
 from openai import OpenAI 
 from scrapper import *
 from dotenv import load_dotenv
+from flask import Flask, render_template, request
 
-def main():
-    url='https://heat.csc.uvic.ca/coview/course/2024091/CSC360?unp=t'
-    filepath=generate_file(url=url)
-    information=read_file(filepath=filepath)
-    generate_response(information)
+app = Flask(__name__)
+
+def main(url):
+    information=fetch_html(url=url)
     response=generate_response(information=information)
     print(response)
-
-def generate_file(url):
-    html_text=fetch_html(url)
-    file_path=make_filepath(url=url)
-    write_file(text=html_text,file_path=file_path)
-    return file_path
-
-def read_file(filepath):
-    with open(filepath,"r",encoding='utf-8') as file:
-        information=file.read()
-    return information
 
 def generate_response(information):
     load_dotenv()
     api_key=os.getenv("API_KEY")
     client=OpenAI(api_key=api_key)
-    prompt="Highligth midterm dates and assignment due dates from this : \n" + information  
+    prompt='''Highlight midterm dates and assignment due dates from this. 
+            Give response as dict with key-value pair. 
+            Key being midterm or assignment and value being the list of dates: \n''' + information  
     chat_completion=client.chat.completions.create(
         messages=[
             {
@@ -40,10 +31,16 @@ def generate_response(information):
     response=chat_completion.choices[0].message.content
     return response
 
-
-
-
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    url = None
+    response=None
+    if request.method == 'POST':
+        url = request.form.get('url')  # Get URL from form
+        if url:
+            response = main(url)  # Call main function with the URL
+    return render_template('index.html', url=url, response=response)
 
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
